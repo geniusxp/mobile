bf6b2cd5b8fe8762779d07a809614e10ca70dfba
ericknathan <erick.capito@hotmail.com>
2024-08-16 11:30:19 -0300

    refactor: migrate activity to appcompatactivity

--- app/src/main/java/com/github/ericknathan/geniusxp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/github/ericknathan/geniusxp/ui/activity/MainActivity.kt
@@ -2,10 +2,11 @@ package com.github.ericknathan.geniusxp.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.github.ericknathan.geniusxp.R
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
