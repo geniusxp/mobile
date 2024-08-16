c7fc6aaadae2fb1a28d2ffb113c7c2d76ec80e94
ericknathan <erick.capito@hotmail.com>
2024-08-16 11:45:20 -0300

    refactor: undo appcompactactivity migration

--- app/src/main/java/com/github/ericknathan/geniusxp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/github/ericknathan/geniusxp/ui/activity/MainActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.github.ericknathan.geniusxp.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         installSplashScreen()
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_recover_password)
     }
 }
