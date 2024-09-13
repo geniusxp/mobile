37eef47b20a0aee594d3e9c9f75b5c8446f9863b
ericknathan <erick.capito@hotmail.com>
2024-09-12 22:58:49 -0300

    feat: start chat message feature

--- app/src/main/java/com/github/ericknathan/geniusxp/ui/activity/EventActivity.kt
+++ app/src/main/java/com/github/ericknathan/geniusxp/ui/activity/EventActivity.kt
@@ -7,38 +7,48 @@ import com.github.ericknathan.geniusxp.R
 import com.github.ericknathan.geniusxp.databinding.ActivityEventBinding
 import com.github.ericknathan.geniusxp.ui.fragments.InteractionFragment
 import com.github.ericknathan.geniusxp.ui.fragments.MapFragment
-import com.github.ericknathan.geniusxp.ui.fragments.ProfileFragment
 import com.github.ericknathan.geniusxp.ui.fragments.ScheduleFragment
 import com.github.ericknathan.geniusxp.ui.fragments.SpeakersFragment
-import com.github.ericknathan.geniusxp.ui.fragments.TicketsFragment
 
 class EventActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityEventBinding
+    private lateinit var binding: ActivityEventBinding
+
+    private val scheduleFragment = ScheduleFragment()
+    private val speakersFragment = SpeakersFragment()
+    private val interactionFragment = InteractionFragment()
+    private val mapFragment = MapFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEventBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(ScheduleFragment())
+
+        setCurrentFragment(scheduleFragment)
 
         binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.schedule -> replaceFragment(ScheduleFragment())
-                R.id.speakers -> replaceFragment(SpeakersFragment())
-                R.id.interaction -> replaceFragment(InteractionFragment())
-                R.id.map -> replaceFragment(MapFragment())
+            when (it.itemId) {
+                R.id.schedule -> setCurrentFragment(scheduleFragment)
+                R.id.speakers -> setCurrentFragment(speakersFragment)
+                R.id.interaction -> setCurrentFragment(interactionFragment)
+                R.id.map -> setCurrentFragment(mapFragment)
                 else -> {}
             }
-
             true
         }
-
     }
 
-    private fun replaceFragment(fragment : Fragment) {
+    private fun setCurrentFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
+
+        fragmentManager.fragments.forEach { fragmentTransaction.hide(it) }
+
+        if (fragment.isAdded) {
+            fragmentTransaction.show(fragment)
+        } else {
+            fragmentTransaction.add(R.id.frameLayout, fragment)
+        }
+
         fragmentTransaction.commit()
     }
 }
--- app/src/main/java/com/github/ericknathan/geniusxp/ui/fragments/InteractionFragment.kt
+++ app/src/main/java/com/github/ericknathan/geniusxp/ui/fragments/InteractionFragment.kt
@@ -2,12 +2,23 @@ package com.github.ericknathan.geniusxp.ui.fragments
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TextAppearanceSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.github.ericknathan.geniusxp.R
+import com.github.ericknathan.geniusxp.services.getUserProfile
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.launch
+
 
 class InteractionFragment : Fragment() {
     @SuppressLint("SetJavaScriptEnabled")
@@ -16,9 +27,25 @@ class InteractionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_interaction, container, false)
-        val youtubeVideoId = "7Ggx_UsW17o"
 
         val youtubeWebView = view.findViewById<WebView>(R.id.youtubeWebView)
+        loadYoutubeVideo(youtubeWebView)
+
+        val sendMessageButton = view.findViewById<View>(R.id.sendMessageButton)
+        val messageInput = view.findViewById<EditText>(R.id.messageInput)
+        val messageList = view.findViewById<View>(R.id.messageList)
+
+        sendMessageButton.setOnClickListener {
+            sendMessage(messageInput.text.toString(), messageList)
+            messageInput.text.clear()
+        }
+
+        return view
+    }
+
+    private fun loadYoutubeVideo(youtubeWebView: WebView) {
+        val youtubeVideoId = "7Ggx_UsW17o"
+
         youtubeWebView.settings.javaScriptEnabled = true
         youtubeWebView.settings.loadWithOverviewMode = true
         youtubeWebView.settings.useWideViewPort = true
@@ -30,9 +57,26 @@ class InteractionFragment : Fragment() {
         youtubeWebView.settings.mediaPlaybackRequiresUserGesture = false
 
         youtubeWebView.loadUrl("https://www.youtube.com/embed/$youtubeVideoId?autoplay=1&modestbranding=1&autohide=1&showinfo=0&controls=0")
-
-
-        return view
     }
 
+    private fun sendMessage(message: String, messageListView: View) {
+        lifecycleScope.launch {
+            val user = getUserProfile(requireActivity()) ?: return@launch
+
+            val messageView = layoutInflater.inflate(
+                R.layout.comp_message_item,
+                messageListView as ViewGroup,
+                false
+            )
+            Picasso.get().load(user.avatarUrl)
+                .into(messageView.findViewById<ImageView>(R.id.avatarImage));
+
+            val spannable = SpannableString("${user.name}: $message")
+            val usernameStyles = TextAppearanceSpan(requireContext(), R.style.MessageUsername)
+            spannable.setSpan(usernameStyles, 0, user.name.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            messageView.findViewById<TextView>(R.id.messageText).text = spannable
+            messageListView.addView(messageView)
+        }
+    }
 }
