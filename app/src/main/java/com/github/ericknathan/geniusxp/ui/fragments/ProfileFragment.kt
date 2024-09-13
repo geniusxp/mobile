1eac5efaceb8ebef3c09cbf01f8267a1dc03f568
ericknathan <erick.capito@hotmail.com>
2024-09-13 13:04:56 -0300

    feat: add logout button

--- app/src/main/java/com/github/ericknathan/geniusxp/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/github/ericknathan/geniusxp/ui/fragments/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.github.ericknathan.geniusxp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,13 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.github.ericknathan.geniusxp.R
 import com.github.ericknathan.geniusxp.services.getUserProfile
+import com.github.ericknathan.geniusxp.ui.activity.SignInActivity
+import com.github.ericknathan.geniusxp.ui.activity.WelcomeActivity
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
@@ -21,6 +27,11 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val logoutButton = view.findViewById<TextView>(R.id.logoutButton)
+        logoutButton.setOnClickListener {
+            logout()
+        }
 
         requireActivity().runOnUiThread {
             setUserData(view)
@@ -56,6 +67,20 @@ class ProfileFragment : Fragment() {
                 chip.text = interest
                 interestsView.addView(chip)
             }
+        }
+    }
+
+    private fun logout() {
+        val sharedPreferences = requireActivity().getSharedPreferences("user", 0)
+        sharedPreferences.edit().clear().apply()
+
+        activity?.runOnUiThread {
+            val intent = Intent(activity, WelcomeActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            activity?.startActivity(intent)
+            activity?.finish()
+
+            Toast.makeText(activity, "Logout realizado com sucesso.", Toast.LENGTH_SHORT).show()
         }
     }
 }
